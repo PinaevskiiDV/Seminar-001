d338d743856988a8deaf763327a00d58ae425f9b
Dmitrii <pinochet37@mail.ru>
2022-08-31 20:52:22 +0300

    program creation

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите торое число");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.Write ("Большее число ");
+    Console.WriteLine (a);
+}
+else
+{
+    Console.Write ("Большее число ");
+    Console.WriteLine (b);
+}
